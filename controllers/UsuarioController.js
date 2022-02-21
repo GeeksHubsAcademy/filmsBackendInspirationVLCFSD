5fdf0f651bf36a08258782e31ace5612e505a853
Dave86dev <davidochando1986@gmail.com>
2022-02-21 13:28:17 +0100

    jwt implementado

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -2,6 +2,7 @@
 const { Usuario } = require('../models/index');
 const bcrypt = require('bcrypt');
 const authConfig = require('../config/auth');
+const jwt = require('jsonwebtoken');
 
 const UsuarioController = {};
 
@@ -47,8 +48,10 @@ UsuarioController.registraUsuario = async (req, res) => {
         let surname = req.body.surname;
         let nickname = req.body.nickname;
         let email = req.body.email;
+        console.log("antes de encriptar",req.body.password);
         let password = bcrypt.hashSync(req.body.password, Number.parseInt(authConfig.rounds)); 
         
+        console.log("este es el password", password);
         //Comprobación de errores.....
         
         //Guardamos en sequelize el usuario
@@ -61,7 +64,6 @@ UsuarioController.registraUsuario = async (req, res) => {
             password: password,
             nickname: nickname
         }).then(usuario => {
-            console.log("este es mi amigo", usuario);
             res.send(`${usuario.name}, bienvenida a este infierno`);
         });
 
@@ -134,6 +136,40 @@ UsuarioController.deleteById = async (req, res) => {
 
 UsuarioController.logUsuario = (req, res) => {
 
+    let correo = req.body.email;
+    let password = req.body.password;
+
+    Usuario.findOne({
+        where : {email : correo}
+    }).then(Usuario => {
+
+        if(!Usuario){
+            res.send("Usuario o contraseña inválido");
+        }else {
+            //el usuario existe, por lo tanto, vamos a comprobar
+            //si el password es correcto
+
+            if (bcrypt.compareSync(password, Usuario.password)) { //COMPARA CONTRASEÑA INTRODUCIDA CON CONTRASEÑA GUARDADA, TRAS DESENCRIPTAR
+
+                console.log(Usuario.password);
+
+                let token = jwt.sign({ usuario: Usuario }, authConfig.secret, {
+                    expiresIn: authConfig.expires
+                });
+
+                res.json({
+                    usuario: Usuario,
+                    token: token
+                })
+            } else {
+                res.status(401).json({ msg: "Usuario o contraseña inválidos" });
+            }
+        };
+
+
+    }).catch(error => {
+        res.send(error);
+    })
 };
 
 module.exports = UsuarioController;
--- views/UsuarioRouter.js
+++ views/UsuarioRouter.js
@@ -1,31 +1,32 @@
 
 const express = require('express');
 const router = express.Router();
+const auth = require("../middlewares/auth");
 
 const UsuarioController = require('../controllers/UsuarioController');
 
 //CRUD RESTful
 
 //Leer todos los usuarios
-router.get('/', UsuarioController.traeUsuarios);
+router.get('/', auth,  UsuarioController.traeUsuarios);
 //http://localhost:3000/usuarios
 
 
-router.get('/email/:email', UsuarioController.traerUsuarioEmail);
-router.get('/:id', UsuarioController.traerUsuarioId);
+router.get('/email/:email', auth, UsuarioController.traerUsuarioEmail);
+router.get('/:id', auth, UsuarioController.traerUsuarioId);
 
 //Registro
 router.post('/', UsuarioController.registraUsuario);
 //http://localhost:3000/usuarios
 
 //Modificar datos de un Usuario
-router.put('/:id', UsuarioController.updateProfile);
+router.put('/:id', auth, UsuarioController.updateProfile);
 
 //Borramos a todos los usuarios
-router.delete('/', UsuarioController.deleteAll);
+router.delete('/', auth, UsuarioController.deleteAll);
 
 //Borramos a todos los usuarios
-router.delete('/:id', UsuarioController.deleteById);
+router.delete('/:id', auth, UsuarioController.deleteById);
 
 //Login
 router.post('/login', UsuarioController.logUsuario);
